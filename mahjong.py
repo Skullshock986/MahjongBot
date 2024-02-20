efad95f236bdca223fc8c675f426e0d1d0e19d01
Dova-Pug <samueltaylor1010@gmail.com>
2024-02-20 15:26:45 +0000

    push changed (borken)

--- mahjong.py
+++ mahjong.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 total_tiles = {}
 with open('tiles.json', 'r') as file:
@@ -10,12 +11,22 @@ class Player:
         self._hand = hand #Would be ideal to ensure the list is sorted, either we can implement it into the constructor at this stage or we can check it at the input stage idk which is better
     
     def discard(self, drawnTile: str): #Eventually will be filled with a method to select a discard, then return new hand and the discard tile
-        pass
+        self._hand.append(str) #append 14th tile to hand
+        discardPossibilities = []
+        for i in range(len(self._hand)): 
 
-    def format_hand(self): #The intent is to separate the raw hand into sublists with integer values for each suit and an integer count of the honour tiles
+            tempHand = self._hand[:i] + self._hand[i+1:]
+
+            tempScore = self.format_hand(tempHand)
+            discardPossibilities = (self._hand[i] , tempScore)  
+        
+        discardPossibilities.sort(key=lambda x: (x[1]["shanten"], x[1]["tileEff"]))
+
+
+    def format_hand(self, hand:List[str]) : #The intent is to separate the raw hand into sublists with integer values for each suit and an integer count of the honour tiles
         
 
-        tilesDict = {
+        handDict = {
             "char": [],   # Hopefully this will be useful for calculating shanten and similar operations
             "circ": [],
             "bamb": [],
@@ -28,19 +39,30 @@ class Player:
             "w_drag": 0,
         }
 
-        for i in self._hand:
-            for item in tilesDict:
+        for i in hand:
+            for item in handDict:
                 if item in i:
                     if item in ["char", "circ", "bamb"]:
-                        tilesDict[item].append(int(i[0]))
+                        handDict[item].append(int(i[0]))
                     else:
-                        tilesDict[item] +=1
+                        handDict[item] +=1
 
-        print(tilesDict)
+        
+        handScore = {
+            "hand" : handDict,
+            "shanten" : self.calcShanten(handDict),
+            "tileEff" : self.calcTileEff(handDict)
+        }
 
-    def calcShanten(self):
-        print('cope')
+        return handScore
 
+    def calcShanten(self, handDict):
+        return 1
+
+    def calcTileEff(self, handDict):
+        return 30
+
+    
 
 seq = {
     "1,2,3"
@@ -52,4 +74,4 @@ trip = {
 }
 
 player = Player(["2_char", "3_char",	"3_char",	"2_circ",	"3_circ",	"6_circ",	"6_circ",	"6_bamb",	"7_bamb",	"7_bamb",	"w_drag",	"w_drag",	"w_drag"])
-player.format_hand()
+player.discard("1_char")
