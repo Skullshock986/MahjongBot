b99810095d0121bb0abde12b041f856de58158e4
JoshThanki <joshthanki@gmail.com>
2024-02-21 16:54:53 +0000

    bruh

--- mahjong.py
+++ mahjong.py
@@ -6,6 +6,17 @@ total_tiles = {}
 with open('tiles.json', 'r') as file:
     start_tiles = json.load(file) #Creates a dictionary containing an entire set of tiles
 
+class Game:
+    def __init__(self) -> None:
+        self.tilePool = start_tiles
+
+    def chooseDora(self):
+        # Extract keys and weights from the dictionary
+        keys, weights = zip(*self.tilePool.items())
+
+        # Choose a random element based on weights
+        random_element = random.choices(keys, weights=weights)[0]
+        return(random_element)    
 
 class Player:
     def __init__(self, hand: list) -> None:
@@ -224,3 +235,6 @@ trip = {
 
 player = Player(["2_char", "3_char",	"5aka_char",	"2_circ",	"3_circ",	"6_circ",	"6_circ",	"6_bamb",	"7_bamb",	"7_bamb",	"w_drag",	"w_drag",	"w_drag"])
 player.discard("1_char")
+
+testGame = Game()
+print(testGame.chooseDora())
