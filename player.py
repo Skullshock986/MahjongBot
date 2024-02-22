4ab7200fc884548c73208c58da44c3b547dbefa4
JoshThanki <joshthanki@gmail.com>
2024-02-22 20:54:11 +0000

    Fixed Merge Conflict

--- player.py
+++ player.py
@@ -192,36 +192,21 @@ class Player:
                 out[i] = arr1[i] - arr2[i]
             return out
         
-<<<<<<< Updated upstream
 
-        def splits_nogroups(hand):
-            set_insequences = incomplete_sequences(hand)
-            current_shan=0
-            set_pairs = pairs(hand)
-            pair_bool = False
-
-            for i in set_pairs:
-                pair_bool = True
-                current = splits_nogroups(resulting_hand(hand, i))[0]+1
-=======
         def splits_nogroups(handArray):
             set_insequences = incomplete_sequences(handArray)
             current_shan=0
             set_pairs = pairs(handArray)
-
-            for i in set_insequences:
-                current = splits_nogroups(resulting_hand(handArray, i))+1
-                if current > current_shan:
-                    current_shan = current
+            pair_bool = False
 
             for i in set_pairs:
-                current = splits_nogroups(resulting_hand(handArray, i))+1
->>>>>>> Stashed changes
+                pair_bool = True
+                current = splits_nogroups(resulting_hand(handArray, i))[0]+1
                 if current>current_shan:
                     current_shan = current
 
             for i in set_insequences:
-                current = splits_nogroups(resulting_hand(hand, i))[0]+1
+                current = splits_nogroups(resulting_hand(handArray, i))[0]+1
                 if current > current_shan:
                     current_shan = current
                     pair_bool = False
@@ -229,100 +214,61 @@ class Player:
             return current_shan, pair_bool
 
 
-        def splits(g, handArray):          #******
+        def splits(g, handArray):         
             current_g_n = g
-<<<<<<< Updated upstream
             current_i_n = 0 
             pair_presance = False                            
-            set_pairs = pairs(hand)
-            set_seq = complete_sequences(hand)
-            set_triplets = triplets(hand)
-
-            if len(set_seq) == 0  and len(set_triplets) == 0:
-                return g, splits_nogroups(hand)[0], splits_nogroups(hand)[1]
-=======
-            current_i_n = 0                             
             set_pairs = pairs(handArray)
             set_seq = complete_sequences(handArray)
             set_triplets = triplets(handArray)
 
             if len(set_seq) == 0  and len(set_triplets) == 0:
-                return g, splits_nogroups(handArray)
-
->>>>>>> Stashed changes
+                return g, splits_nogroups(handArray)[0], splits_nogroups(handArray)[1]
 
             for j in set_seq:
                 current = splits(g+1, resulting_hand(handArray, j))[0]
                 if current>current_g_n:
                     current_g_n = current
-<<<<<<< Updated upstream
-                    current_i_n = splits(g+1,resulting_hand(hand, j))[1]
-                    pair_presance = splits(g+1,resulting_hand(hand, j))[2] #*
-                elif current == current_g_n:
-                    if splits(g+1,resulting_hand(hand, j))[1] > current_i_n:
-                        current_i_n = splits(g+1,resulting_hand(hand, j))[1]
-                        pair_presance = splits(g+1,resulting_hand(hand, j))[2] #*
-=======
                     current_i_n = splits(g+1,resulting_hand(handArray, j))[1]
+                    pair_presance = splits(g+1,resulting_hand(handArray, j))[2] #*
                 elif current == current_g_n:
                     if splits(g+1,resulting_hand(handArray, j))[1] > current_i_n:
                         current_i_n = splits(g+1,resulting_hand(handArray, j))[1]
-
->>>>>>> Stashed changes
+                        pair_presance = splits(g+1,resulting_hand(handArray, j))[2] #*
 
             for j in set_triplets:
                 current = splits(g+1,resulting_hand(handArray, j))[0]
                 if current>current_g_n:
                     current_g_n = current
-<<<<<<< Updated upstream
-                    current_i_n = splits(g+1,resulting_hand(hand, j))[1]
-                    pair_presance = splits(g+1,resulting_hand(hand, j))[2] #*
+                    current_i_n = splits(g+1,resulting_hand(handArray, j))[1]
+                    pair_presance = splits(g+1,resulting_hand(handArray, j))[2] #*
                 elif current == current_g_n:
-                    if splits(g+1,resulting_hand(hand, j))[1] > current_i_n:
-                        current_i_n = splits(g+1,resulting_hand(hand, j))[1]
-                        pair_presance = splits(g+1,resulting_hand(hand, j))[2] #*
+                    if splits(g+1,resulting_hand(handArray, j))[1] > current_i_n:
+                        current_i_n = splits(g+1,resulting_hand(handArray, j))[1]
+                        pair_presance = splits(g+1,resulting_hand(handArray, j))[2] #*
 
             return current_g_n,current_i_n,pair_presance
         
 
-        def splits_fullhand(hand):
+        def splits_fullhand(handArray):
             current_split = [0,0,False]
-            for i in hand[:3]:
+            for i in handArray[:3]:
                 current_arr = splits(0, i)
                 current_split[0] += current_arr[0]
                 current_split[1] += current_arr[1]
                 if current_arr[2] == True:
                     current_split[2] = True
 
-            for i in hand[3]:
-=======
-                    current_i_n = splits(g+1,resulting_hand(handArray, j))[1]
-                elif current == current_g_n:
-                    if splits(g+1,resulting_hand(handArray, j))[1] > current_i_n:
-                        current_i_n = splits(g+1,resulting_hand(handArray, j))[1]
-            return current_g_n, current_i_n
-    
-        def splits_fullhand(handArray):
-            current_split = [0,0]
-            for i in handArray[:3]:
-                current_split[0] += splits(0, i)[0]
-                current_split[1] += splits(0, i)[1]
             for i in handArray[3]:
->>>>>>> Stashed changes
                 if i == 3:
                     current_split[0] += 1
                 elif i == 2:
                     current_split[1] +=1
             return current_split
 
-<<<<<<< Updated upstream
 
-        def shanten(hand):
-            split_arr = splits_fullhand(hand)
-=======
         def shanten(handArray):
             split_arr = splits_fullhand(handArray)
->>>>>>> Stashed changes
             i = split_arr[1]
             g = split_arr[0]
             pair_presence = split_arr[2]
@@ -332,17 +278,10 @@ class Player:
                 return 1
             return 8 - 2*g - min(i, 4-g) - min(1, max(0,i-4+g))
         
-<<<<<<< Updated upstream
 
-        return shanten(hand)
+        return shanten(handArray)
     
 
 
     def calcTileEff(self, hand):
-=======
-        return shanten(handArray)
-    
-
-    def calcTileEff(self, handArray):
->>>>>>> Stashed changes
         return random.randint(1,30)
